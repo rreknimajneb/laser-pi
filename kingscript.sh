aa8e76529e8747735b4f82f8e468029f81f84289
rreknimajneb <rreknimajneb@yahoo.com>
2013-12-16 08:49:55 +0000

    added gpio LED & button and display scripting(not functional yet)

--- kingscript.sh
+++ kingscript.sh
@@ -1,12 +1,20 @@
-# check for dependencies
-which fbi
-# which fbsetup
-which gpio
-which convert #which imagemagick
-which imagemagick
-which raspistill
-which fswebcam
+echo " check for dependencies"
 
+echo "fbi = " `which fbi`
+#-z unary test for nul string#
+#
+echo "fbset = " ` which fbset`
+echo "gpio = " `which gpio`
+echo "convert = " `which convert`
+echo "imagemagick = " `which imagemagick`
+echo "raspistill = " `which raspistill`
+echo "fswebcam = " `which fswebcam`
+
+echo "find the screen"
+SCRN=`ls /dev/fb*`
+echo "frame buffer = ?? "$SCRN
+DISP=`env | grep DISPLAY`
+echo "display? = "$DISP
 #start.sh
 
 #snaps.sh
@@ -15,6 +23,12 @@ which fswebcam
 
 #fbi -d /dev/fb0 #$pic #if launched directly from pi
 #else
-#fbi -T 0 $pic
+sudo fbi -T 1 -v $1
+
+
+# fbi OPTIONS
+#       -d device
+#             framebuffer device to use.  Default is the one your vc is mapped to.
+
 
 
